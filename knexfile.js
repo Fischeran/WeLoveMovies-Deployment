c3ee28bb28abecda7c90de5dc973e0a1603cfb97
Alex <alnfischer@gmail.com>
2022-11-09 18:53:24 -0500

    env variables

--- knexfile.js
+++ knexfile.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 
 const {
   DATABASE_URL = "postgresql://postgres@localhost/postgres",
+  PRODUCTION_DATABASE_URL = "postgresql://postgres@localhost/postgres"
 } = process.env;
 
 module.exports = {
@@ -21,7 +22,7 @@ module.exports = {
 
   production: {
     client: "postgresql",
-    connection: DATABASE_URL,
+    connection: PRODUCTION_DATABASE_URL,
     pool: { min: 0, max: 5 },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
